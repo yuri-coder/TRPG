aa3e86dbd0728021cfdec29bf93a9e85dad9f49e
yuri-coder <eirika.sawh@gmail.com>
2018-05-17 21:55:45 -0400

    small push
    + itty bitty beep bop push

--- Assets/Scripts/Attacks/Attack.cs
+++ Assets/Scripts/Attacks/Attack.cs
@@ -7,6 +7,12 @@ public abstract class Attack {
     public string attackName; //Name of the attack
     public string description; //Flavor text description of the attack
     public int attackID; //Unique ID of the attack
-	
+    public ElementalTypes elementType; //Type of attack
+    public bool isPhysical; //True if physical, false if magical
+    public double accuracy; //Given as whole % value (10 as opposed to 0.1)
+
+    
+
+
 }
 
